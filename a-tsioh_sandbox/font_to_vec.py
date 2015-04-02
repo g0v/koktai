7ae90cb159cf84d91e612bd6016c50b4178ff2c1
a-tsioh <pmagistry@gmail.com>
2015-04-02 18:44:58 +0200

    unlucky tries

--- a-tsioh_sandbox/font_to_vec.py
+++ a-tsioh_sandbox/font_to_vec.py
@@ -6,17 +6,19 @@ import ImageDraw
 import ImageFont
 import json
 
-SIZE = (10,10)
+from png_to_vec import scan_lines_and_columns
+
+SIZE = (60,60)
 
 chars = [x.decode('utf8').strip() for x in open("unicode_ext_charlist.txt").readlines()]
 
 data = {}
 for text in chars:
     ttfont = ImageFont.truetype ('/usr/share/fonts/truetype/arphic/uming.ttc', 128)
-    image = Image.new ('1', (128, 128), 1);
-    ImageDraw.Draw(image).text( (0, 0), text, font = ttfont, fill = 0 )
+    image = Image.new ('1', (128, 128), 0);
+    ImageDraw.Draw(image).text( (0, 0), text, font = ttfont, fill = 1 )
     small = image.resize(SIZE)
-    data["%x"%(ord(text),)] = list(small.im)
+    data["%x"%(ord(text),)] = small.histogram()
 
 
 print json.dumps(data)
--- a-tsioh_sandbox/png_to_vec.py
+++ a-tsioh_sandbox/png_to_vec.py
@@ -4,13 +4,26 @@ import os
 import json
 
 
-SIZE=(10,10)
+SIZE=(60,60)
 
+def scan_lines_and_columns(vect):
+    result = [0] * (SIZE[0] + SIZE[1])
+    for x in range(SIZE[0]):
+        for y in range(SIZE[1]):
+            if vect[x+SIZE[1]*y]:
+                result[y] += 1
+                result[SIZE[1]+x] += 1
+    return result
+
+def distance(h1,h2):
+    rms = math.sqrt(reduce(operator.add,map(lambda a,b: (a-b)**2, h1, h2))/len(h1))
+    return rms
 
 def compute_vec(path):
     im = Image.open(path)
     small = im.resize(SIZE)
-    return list(small.im)
+    return small.histogram()
+    return scan_lines_and_columns(list(small.im))
 
 
 def main(folder):

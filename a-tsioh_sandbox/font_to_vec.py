2c5f8d441fc08d9973b2dc488b9d8b6f5b6254bd
a-tsioh <pmagistry@gmail.com>
2015-04-02 16:56:03 +0200

    generate vecs from font and charlist

---
+++ a-tsioh_sandbox/font_to_vec.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import Image
+import ImageDraw
+import ImageFont
+import json
+
+SIZE = (10,10)
+
+chars = [x.decode('utf8').strip() for x in open("unicode_ext_charlist.txt").readlines()]
+
+data = {}
+for text in chars:
+    ttfont = ImageFont.truetype ('/usr/share/fonts/truetype/arphic/uming.ttc', 128)
+    image = Image.new ('1', (128, 128), 1);
+    ImageDraw.Draw(image).text( (0, 0), text, font = ttfont, fill = 0 )
+    small = image.resize(SIZE)
+    data["%x"%(ord(text),)] = list(small.im)
+
+
+print json.dumps(data)

5b5980ec6b2dcbe031c871512cb9c689902d47e7
Audrey Tang <audreyt@audreyt.org>
2015-04-02 21:33:41 +0800

    * script to convert cp950-area-unicode-PUA into U+Fxyzw PUA

--- a-tsioh_sandbox/recode_utf8.pl
+++ a-tsioh_sandbox/recode_utf8.pl
@@ -1,7 +1,14 @@
 use Encode qw(decode encode);
 
 while(<>){
-    print encode('UTF-8', decode('CP950', $_));
-    #print encode('UTF-8', decode('MacChineseTrad', $_));
+    print encode('UTF-8', remap_pua(decode('CP950', $_)));
 }
 
+sub remap_pua {
+    # Take BMP PUAs and make them "go astral" using original Big5 codepoints
+    my $str = shift;
+    $str =~ s!([\x{E000}-\x{F8FF}])!
+        chr(0xF0000 + hex(join '', map sprintf("%02X", ord $_), split //, encode('CP950' => $1)))
+    !egx;
+    return $str;
+}

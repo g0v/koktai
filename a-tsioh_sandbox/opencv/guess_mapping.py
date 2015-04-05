db815e55a51e4b13543a59b6406145c4f7a7f641
a-tsioh <pmagistry@gmail.com>
2015-04-05 22:15:43 +0200

    playing with preprocessing

--- a-tsioh_sandbox/opencv/guess_mapping.py
+++ a-tsioh_sandbox/opencv/guess_mapping.py
@@ -5,29 +5,58 @@ import cv2
 import numpy as np
 
 
-NBESTS = 10
+NBESTS = 5
 
-matcher = cv2.BFMatcher()
+matcher = cv2.BFMatcher(crossCheck=False)
+#matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 sift = cv2.SIFT()
-kernel = np.ones((10,10),np.float32)/(10*10)
+#orb = cv2.ORB()
+kernel = np.ones((3,3),np.float32)/(3*3)
+dilate_kernel = np.ones((3,1),np.uint8)
 
 def compare_two(des1,des2):
-    return np.mean([x.distance for x in matcher.match(des1,des2)])
+    if des1 is None or des2 is None:
+        return np.float("inf")
+    N = int(len(des1)*0.95)
+    distsA = sorted([x.distance for x in matcher.match(des1,des2)])[:N]
+    if len(distsA) == 0 :
+        return np.float('inf')
+    if len(distsA) < N:
+        distsA.extend([distsA[-1]*1.3]*(N-len(distsA)))
+    #N2 = int(N*0.8) #int(len(des1)*1.0)
+    #distsB = sorted([x.distance for x in matcher.match(des2,des1)])[:N2]
+    #if len(distsB) == 0 :
+    #    return np.float('inf')
+    #if len(distsB) < N2:
+    #    distsB.extend([distsB[-1]*1.3]*(N2-len(distsB)))
+    return np.nanmean(distsA)
 
-def compute_descriptions(paths):
+def compute_descriptions(paths, dilate=False):
     result = {}
     for p in paths:
         im = cv2.imread(p)
-        #edges = np.uint8(np.absolute(cv2.Laplacian(im, cv2.CV_64F)))
-        blurry = cv2.filter2D(im,-1,kernel)
         
+        #blur
+        im = cv2.filter2D(im,-1,kernel)
+
+        #dilate
+        if dilate:
+            im = cv2.dilate(im, dilate_kernel, iterations=1)
+        #else:
+        #    im = cv2.erode(im, kernel3, iterations=1)
+        
+        #thresholding
+        _, im = cv2.threshold(im, 200, 255, cv2.THRESH_BINARY)
+        
+        #im = np.uint8(np.absolute(cv2.Laplacian(im, cv2.CV_64F)))
         #gray = cv2.cvtColor(blurry, cv2.COLOR_BGR2GRAY)
-        kp, des = sift.detectAndCompute(blurry, None)
+        kp, des = sift.detectAndCompute(im, None)
+        #kp, des = orb.detectAndCompute(im, None)
         result[p] = des
     return result
 
 def main(sources, targets):
-    s_desc = compute_descriptions(sources)
+    s_desc = compute_descriptions(sources, dilate=True)
     t_desc = compute_descriptions(targets)
     results = {}
     print "<html><body>"

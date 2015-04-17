ac94abba092b98293f48e3ea22a10ae59ca78c41
Audrey Tang <audreyt@audreyt.org>
2015-04-18 04:47:20 +0800

    * Regen HTML, for real.

--- font/jade-unescape.pl
+++ font/jade-unescape.pl
@@ -6,17 +6,20 @@ use FindBin '$Bin';
 
 my $k = decode_json(slurp("$Bin/k.json"));
 my $m3 = decode_json(slurp("$Bin/m3.json"));
+my $mapping = decode_json(slurp("$Bin/../a-tsioh_sandbox/mapping.json"));
 
 while (<>) {
     Encode::_utf8_on($_);
     s{<k>(.*?)</k>}{k($1)}eg;
-    s!&#xFc6a([1-9]);!chr(0x245f + $1)!eg;
-    s!&#xF(c...);!
-        $m3->{$1} ? qq[$m3->{$1}] : qq[<img src="img/m3/$1.png">]
-    !eg;
-    s!&#xF(....);!
-        $m3->{$1} ? qq[<rt>$m3->{$1}</rt>] : qq[<img src="img/m3/$1.png">]
-    !eg;
+    s|([\x{Fc6a1}-\x{Fc6a9}])(?!</mark>)(?!</rt>)|chr(0x245f + ord($1) - 0xFc6a0)|eg;
+    s|([\x{Fc000}-\x{Fcfff}])(?!</mark>)(?!</rt>)|
+        my $code = sprintf('%04x', ord($1) - 0xF0000);
+        $m3->{$code} ? qq[$m3->{$code}] : qq[<img src="img/m3/$code.png">]
+    |eg;
+    s|([\x{F0000}-\x{Fffff}])(?!</mark>)(?!</rt>)|
+        my $code = sprintf('%04x', ord($1) - 0xF0000);
+        $m3->{$code} ? qq[<rt>$m3->{$code}</rt>] : qq[<img src="img/m3/$code.png">]
+    |eg;
     Encode::_utf8_off($_);
     s/^html$/html(lang="zh-Hant-TW")/;
     print;
@@ -27,12 +30,14 @@ while (<>) {
 
 sub k {
     my $str = shift;
-    $str =~ s!&#xFc6a([1-9]);!chr(0x245f + $1)!eg;
-    $str =~ s!&#xF([89af]...);!
-        $k->{$1} ? qq[<rt>$k->{$1}</rt>] : qq[<mark>&#xf$1;</mark>]
+    $str =~ s!([\x{Fc6a1}-\x{Fc6a9}])!chr(0x245f + ord($1) - 0xFc6a0)!eg;
+    $str =~ s!([\x{F8000}-\x{Fafff}\x{Ff000}-\x{Fffff}])!
+        my $code = sprintf('%04x', ord($1) - 0xF0000);
+        $mapping->{$1} || ($k->{$code} ? qq[<rt>$k->{$code}</rt>] : qq[<mark>&#xf$code;</mark>])
     !eg;
-    $str =~ s!&#xF([^89af]...);!
-        $k->{$1} ? qq[<rt>$k->{$1}</rt>] : qq[<img src="img/k/$1.png">]
+    $str =~ s!([\x{F0000}-\x{F7fff}\x{Fb000}-\x{Fefff}])!
+        my $code = sprintf('%04x', ord($1) - 0xF0000);
+        $mapping->{$1} || ($k->{$code} ? qq[<rt>$k->{$code}</rt>] : qq[<img src="img/k/$1.png">])
     !eg;
     return $str;
 }

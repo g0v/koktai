346abe8ed7545ce0f30d0e30b481a83761102cab
a-tsioh <pmagistry@gmail.com>
2015-05-01 17:42:52 +0800

    sketch post proc for {{rt|}} templating

---
+++ font/rt-template.pl
@@ -0,0 +1,15 @@
+use strict;
+
+use Encode;
+
+
+
+
+while (<>) {
+    Encode::_utf8_on($_);
+    s%<rt>([\x{3105}-\x{31b3}]+)(.?.?)</rt>%\{\{rt\|$1|$2\}\}%g;
+    s/\|\}\}/\}\}/g;
+    print;
+
+
+}

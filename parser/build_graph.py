4970aa80792da1b84641bb6a21bd855c0833a935
a-tsioh <pmagistry@gmail.com>
2016-02-20 12:16:43 +0800

    .dic to Neo4J

---
+++ parser/build_graph.py
@@ -0,0 +1,137 @@
+# -*- coding: utf8 -*-
+
+import py2neo
+import pickle
+import re
+
+from parser import Chapter, Char, Word, parse_one_word, remove_formatting_chars
+
+GRAPH = py2neo.Graph()
+
+re_zhuyin = re.compile("<rt>.*?</rt>")
+def remove_zhuyin(s):
+    return re_zhuyin.sub("",s)
+
+def encode_dict(d):
+    for k,v in d.items():
+        if type(v) is unicode:
+            d[k] = remove_formatting_chars(v)
+    return d
+
+
+chpt_id = 0
+def extract_chapter_props(chpt):
+    global chpt_id
+    chpt_id += 1
+    return encode_dict({u"SID": u"Koktai-chpt-%s-%s-%d" %(chpt.zhuyin, chpt.pinyin,chpt_id),
+            u"label": chpt.zhuyin, 
+            u"注音": chpt.zhuyin,
+            u"pinyin": chpt.pinyin,
+            u"本文": u"".join(chpt.content)})
+
+char_id = 0
+def extract_char_props(char):
+    global char_id
+    char_id += 1
+    return encode_dict({u"SID": u"Koktai-char-%s-%d" %(char.hanji, char_id),
+            u"label": char.hanji.replace("<CHAR/>",""),
+            u"字": unicode(char.hanji.replace("<CHAR/>","")),
+            u"反切": unicode(char.details),
+            u"本文": u"".join(char.content)})
+
+reading_id = 0
+def extract_reading_props(cid, r):
+    global reading_id
+    reading_id += 1
+    return encode_dict({u"SID": "Koktai-reading-%s-%s-%s-%d" % (cid, r[0],r[1], reading_id),
+        u"label": r[1].replace("<rt>","").replace("</rt>", ""),
+        u"參考書": r[0],
+             u"本文": r[1]})
+
+word_id = 0
+def extract_word_props(word):
+    global word_id
+    word_id += 1
+    w = parse_one_word([remove_formatting_chars(l) for l in word.content])
+    return {u"SID": u"Koktai-word-%s-%d" % (w['entry'], word_id),
+            u"label": remove_zhuyin(w['entry']),
+            u"項": w['entry'],
+            u"nhyp": w['nh'],
+            u"詞類": w["POS"],
+            u"本文": w["body"]}
+
+def create_reading(tx, char_ID, reading):
+    req = u"""
+    MATCH (char:字:Koktai {SID:{id}})
+    CREATE (im:音:Koktai)
+    SET im = {props}
+    CREATE (char) -[:唸]-> (im)
+    """
+    props = extract_reading_props(char_ID, reading)
+    tx.append(req, {"props":props, "id": char_ID})
+
+
+
+def create_word(tx, char_ID, word):
+    req = u"""
+    MATCH (char:字:Koktai {SID:{id}})
+    CREATE (w:詞:Koktai)
+    SET w = {props}
+    CREATE (char) -[:屬於]-> (w)
+    """
+    props = extract_word_props(word)
+    tx.append(req, {"props":props, "id": char_ID})
+
+
+
+def create_char(tx, chpt_ID, char):
+    req = u"""
+    MATCH (chpt:章:Koktai {SID:{id}})
+    CREATE (ji:字:Koktai)
+    SET ji = {props}
+    CREATE (chpt) -[r:含]-> (ji)
+    """
+    props = extract_char_props(char)
+    tx.append(req, {"props":props, "id": unicode(chpt_ID)})
+    for r in char.readings:
+        create_reading(tx, props["SID"], r)
+    for w in char.words:
+        create_word(tx, props["SID"], w)
+
+
+def create_chapter(tx, chpt):
+    req = u"""
+    CREATE (chpt:章:Koktai)
+    SET chpt = {props}
+    """
+    props = extract_chapter_props(chpt)
+    tx.append(req, {"props":props})
+    for char in chpt.chars:
+        #print char.hanji.encode("utf8")
+        create_char(tx, props["SID"], char)
+
+
+def create_indexes():
+    try:
+        GRAPH.cypher.execute("""
+        CREATE CONSTRAINT ON (n:Koktai) ASSERT n.SID IS UNIQUE
+        """)
+    except:
+        print "index creation failed (maybe it already exists)"
+
+
+def main(path):
+    with open(path) as F:
+        data = pickle.load(F)
+        create_indexes()
+        tx = GRAPH.cypher.begin()
+        for chpt in data:
+            create_chapter(tx, chpt)
+        tx.commit()
+
+
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1])
+
--- parser/parser.py
+++ parser/parser.py
@@ -4,7 +4,8 @@ from collections import namedtuple
 import re
 import json
 
-
+#QUESTIONS:
+# bb1, fd0
 VERBOSE = True
 
 def debug(x):
@@ -13,8 +14,10 @@ def debug(x):
 
 CHAPTER = u".章首" # header of a chapter, the following line contains a syllable in zhuyin
 ENTRY = u".本文" # start and end of an entry ( one character + words)
-CHAR = u"~fm7t168bb1;" # char entry followed by char, zhuyin and 反切
-CHAR2 = u"~fkt168bb1;" # char entry followed by char, zhuyin and 反切
+#CHAR = u"~fm7t168"#bb1;" # char entry followed by char, zhuyin and 反切
+#CHAR2 = u"~fkt168"#bb1;" # char entry followed by char, zhuyin and 反切
+CHAR_PRE_RECODE_re = re.compile("^~(fm7|fk|fm3)t168")
+CHAR_re = re.compile("^<CHAR/>")
 READING = u"~fm7;" # various readings of a char (mandarin, various hokkien accents, wen/bai)
 
 WORD = u"~t96;" # start of a Word entry (untill next empty line or ENTRY)
@@ -24,26 +27,104 @@ Chapter = namedtuple('Chapter',"zhuyin pinyin content chars words")
 Char = namedtuple('Char', "hanji details readings content words")
 Word = namedtuple('Word', "content")
 
-
 private_to_unicode = json.load(open(os.path.dirname(__file__) + "/mapping.json"))
+m3_mapping = json.load(open(os.path.dirname(__file__) + "/m3.json"))
+k_mapping = json.load(open(os.path.dirname(__file__) + "/k.json"))
+
 re_fk = re.compile(ur"<k>.*?</k>", re.U)
 re_change_font = re.compile(ur"~fk[a-z0-9]*;(.*?)~fm3[a-z0-9]*;",re.U)
 
 def get_one_line(buffer):
     try:
-        return buffer.pop(0)
+        line = buffer.pop(0)
+        #debug(line)
+        return line
     except:
         return None
 
 
+def match_apply(re, f, s):
+    """
+    apply f on every match of re in s
+    """
+    m = re.search(s)
+    output = []
+    while m is not None:
+        #debug("all = " + s)
+        (begin, end) = m.span()
+        output.append(s[:begin])
+        output.append(f(s[begin:end]))
+        #debug("sub = " + output[-1])
+        s = s[end:]
+        m = re.search(s)
+    output.append(s)
+    return "".join(output)
+
+k_re = re.compile(ur"<k>.*?<\/k>",re.U)
+k1_re = re.compile(ur"[\U000fc6a1-\U000fc6a9]", re.U)
+k2_re = re.compile(ur"[\U000F8000-\U000Fafff\U000FF000-\U000FFFFF]", re.U)
+k3_re = re.compile(ur"[\U000F0000-\U000F7fff\U000Fb000-\U000FeFFF]", re.U)
+k4_re = re.compile(ur"[\U000fc6a1-\U000fc6a9](?!</mark>)(?!</rt>)", re.U)
+k5_re = re.compile(ur"[\U000Fc000-\U000Fcfff](?!</mark>)(?!</rt>)", re.U)
+k6_re = re.compile(ur"[\U000F0000-\U000Fffff](?!</mark>)(?!</rt>)", re.U)
+def recode(s):
+    if CHAR_PRE_RECODE_re.match(s):
+        s = "<CHAR/>" + s
+    s = re_change_font.sub("<k>\\1</k>",s)
+    def k1(s):
+        return match_apply(k1_re, lambda x: unichr(0x245f + ord(x) - 0xfc6a0), s[3:-4]) # remove </k>
+    def k2(s):
+        def f(sub):
+            code = "%04x" %(ord(sub) - 0xF0000,)
+            if code in private_to_unicode:
+                return private_to_unicode[code]
+            if code in k_mapping:
+                return "<rt>%s</rt>" % (k_mapping[code],)
+            return "<mark>&#xf%s;</mark>" % (code,)
+        return match_apply(k2_re, f, s)
+    def k3(s):
+        def f(sub):
+            code = "%04x" % (ord(sub) - 0xF0000,)
+            if code in private_to_unicode:
+                return private_to_unicode[code]
+            if code in k_mapping:
+                return "<rt>%s</rt>" % (k_mapping[code],)
+            return "<img src=\"img/k/%s.png\" />" % (sub,)
+        return match_apply(k3_re, f, s)
+    def k5(s):
+        def f(sub):
+            code = "%04x" %(ord(sub) - 0xF0000,)
+            if code in m3_mapping:
+                return m3_mapping[code]
+            return "<img src=\"img/m3/%s.png\" />" % (code,)
+        return match_apply(k5_re, f, s)
+    def k6(s):
+        def f(sub):
+            code = "%04x" %(ord(sub) - 0xF0000,)
+            if code in m3_mapping:
+                return "<rt>%s</rt>" % (m3_mapping[code],)
+            return "<img src=\"img/m3/%s.png\" />" % (code,)
+        return match_apply(k6_re, f, s)
+
+    s = match_apply(k_re, lambda x: k3(k2(k1(x))), s)
+    s = match_apply(k4_re, lambda x: unichr(0x245f + ord(x) - 0xfc6a0), s)
+    return k6(k5(s))
+
+
+re_special_chars = re.compile(ur"~[a-z0-9]+;",re.U)
+def remove_formatting_chars(s):
+    return re_special_chars.sub("",s)
+        
+
+
 def parse_chapter(line, buffer):
-    m = re.match(ur"^(.*)~t112fd0;\[(.*)\]", line, re.UNICODE)
+    m = re.match(ur"^(.*)~t112(?:fd0)?;(\[[^\]]+\])?$", line.strip(), re.UNICODE)
     chp = Chapter(m.group(1), m.group(2),[], [], [])
     return chp
 
     
 def parse_char_reading(line):
-    m = re.match(ur"^~fm7;([^~]+)~fm3t42;　~t84;(.*)$", line)
+    m = re.match(ur"^~fm7;([^~]+)~fm3t42;　(.*)$", line)
     return (m.group(1),m.group(2))
 
 
@@ -77,10 +158,21 @@ def parse_one_word(lines):
     """
     analyse one line, assume unicode
     """
-    re_main_parts = re.compile(ur"^~t96;【(?P<entry>[^】]+)】~(fd6)?t84;(?P<definition>.*)$",re.U)
+    def merge_lines(lines):
+        result = [""]
+        while len(lines) != 0:
+            l = lines.pop(0)
+            if l != "":
+                result[-1] += l
+            else:
+                result.extend(lines)
+                lines = []
+        return result
+    re_main_parts = re.compile(ur"^【(?P<entry>[^】]+)】(?P<definition>.*)$",re.U)
     re_special_chars = re.compile(ur"~[a-z0-9]+;",re.U)
     re_definition = re.compile(ur"^(?P<nhomonym>[0-9]+ )?(?P<POS>\[[^\]]+\])?(?P<body>.*)$", re.DOTALL)
 
+    lines = merge_lines(lines)
     matchs = re_main_parts.match(lines[0])
     if matchs:
         entry = matchs.group("entry")
@@ -91,7 +183,6 @@ def parse_one_word(lines):
         # entry = re_special_chars.sub("", entry)
         # definition = re_special_chars.sub("", definition)
         # analyse def content
-        print definition.encode("utf8")
         def_matchs = re_definition.match(definition)
         if def_matchs:
             nh = def_matchs.group('nhomonym')
@@ -138,9 +229,19 @@ def parse_words(buffer):
 
 
 def parse_char(line, buffer):
-    assert(line.startswith(CHAR) or line.startswith(CHAR2))
-    form, details = line.split("~fm3t84bb1;",1)
-    char = Char(form.replace(CHAR,"").replace(CHAR2,""), details, [], [], [])
+    assert(CHAR_re.match(line))
+    if "~fm3t84bb1;" in line:
+        form, details = line.split("~fm3t84bb1;",1)
+    elif "~fm3t84;" in line:
+        form, details = line.split("~fm3t84;",1)
+    elif "~t84" in line:
+        form, details = line.split("~t84;",1)
+    elif u"　" in line:
+        form, details = line.split(u"　",1)
+    else:
+        form = line.strip()
+        details = ""
+    char = Char(form, details, [], [], [])
     return char
 
 
@@ -158,23 +259,28 @@ def parse(buffer):
             results.append(current_chpt)
             current_section = "chapter"
             current_char = None
-        elif line.startswith(ENTRY) and current_section == "chapter":
-            assert get_one_line(buffer).strip() == ""
-            assert get_one_line(buffer).strip() == ENTRY
+#        elif line.startswith(ENTRY) and current_section == "chapter":
+#            assert get_one_line(buffer).strip() == ""
+#            assert get_one_line(buffer).strip() == ENTRY
+#            current_section = "char"
+#            char = parse_char(get_one_line(buffer), buffer)
+#            current_chpt.chars.append(char)
+#            current_char = char
+        #elif line.startswith(ENTRY) and current_section is None:
+        elif CHAR_re.match(line):
             current_section = "char"
-            char = parse_char(get_one_line(buffer), buffer)
-            current_chpt.chars.append(char)
-            current_char = char
-        elif line.startswith(ENTRY) and current_section is None:
-            current_section = "char"
-            current_char = parse_char(get_one_line(buffer), buffer)
+            current_char = parse_char(line, buffer)
             current_chpt.chars.append(current_char)
         elif line.startswith(READING):
             if not current_section == "char":
                 debug("??? " + str(current_section) + " "+line)
-            current_char.readings.append(parse_char_reading(line))
-        elif line.startswith(ENTRY) and current_section in ("char","word"):
-            current_section = None
+                debug(current_chpt.zhuyin)
+            else:
+                current_char.readings.append(parse_char_reading(line))
+        #elif line.startswith(ENTRY) and current_section in ("char","word"):
+        #    current_section = None
+        elif line.startswith(ENTRY):
+            pass
         elif line.startswith(WORD):
             current_word = Word([line])
             if current_char is not None:
@@ -196,15 +302,32 @@ def parse(buffer):
     return results
 
 
+def str_of_char(c):
+    return (u"%s\n%s\n%s\n---" % ( c.hanji, c.details, "\n".join(["%s: %s"% r for r in c.readings]))).encode("utf8")
+
+def str_of_word(w):
+    return ("""
+    %(entry)s %(nh)s. [%(POS)s]
+    %(body)s
+    ---
+    """ % w).encode("utf8")
+
+
+
 if __name__ == "__main__":
     import sys
     import codecs
     import pickle
-    data = [l.decode("utf8") for l in sys.stdin.readlines()]
+    data = [recode(l.decode("utf8")) for l in sys.stdin.readlines()]
     result = parse(data)
-    for c in result[0].chars:
-        for w in c.words:
-            print "\n".join(w.content)
-            print parse_one_word(w.content)
-            print "----"
-    #pickle.dump(parse(data), open(sys.argv[1],"w"))
+    nc = 0
+    nw = 0
+    for chpt in result:
+        for c in chpt.chars:
+            #print str_of_char(c)
+            nc += 1
+            for w in c.words:
+                #str_of_word(parse_one_word(w.content))
+                nw += 1
+    print nc, nw
+    pickle.dump(result, open(sys.argv[1],"w"))
